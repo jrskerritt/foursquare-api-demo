208d99bc26266ecd1e3287d0822d8cd35ac30072
Jim Skerritt <jrskerritt@gmail.com>
2020-04-12 17:52:05 -0700

    fix port issue

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var searchVenuesHandler = require('./handlers/searchVenuesHandler');
 var app = express();
-var port = 3001;
+var port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
